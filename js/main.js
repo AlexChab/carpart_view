4aac3c33d731e4a31bf0360eaa63048e8e6a1edf
Alex Chaban <alex_hourse@ukr.net>
2016-07-22 17:02:34 +0300

    work-220716-1700

--- js/main.js
+++ js/main.js
@@ -7,6 +7,20 @@ $(document).ready(function(){
 		$("#my-garage").removeClass("fa-chevron-down");
 	  $("#my-garage").addClass("fa-chevron-up");
 	})
+
+	// delete my-car garage
+	$('.del-my-car').on('click',function(){
+		
+		$('#smallModal').modal('toggle');
+		
+		console.log(this.id);
+
+		console.log('del car');
+		$('#form-'+this.id).remove();
+	})
+
+
+	// -- Hover на изменение вида 
 	$('.category').hover(function(){
 		console.log();
 
@@ -16,4 +30,8 @@ $(document).ready(function(){
 		console.log('by');
 	}
 	)
+});
+$(window).load(function(){
+	console.log('onload');
+
 });
